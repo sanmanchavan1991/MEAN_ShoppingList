a78a6c45fc8c8fe4b765b84f6dd526a15d3c3d17
sanman chavan <sanmanchavan1991@gmail.com>
2021-08-04 10:15:26 +0530

    deploy commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import path from 'path';
+
 const app=express();
 
 const itemRoutes =require('./routes/api/items');
@@ -17,6 +19,14 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true })
 
 //use routes
 app.use('/api/items', itemRoutes);
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port=process.env.PORT||5000;
 app.listen(port,()=>console.log(`server started on port${port}`));
